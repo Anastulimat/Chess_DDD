43726ba9c2ad2ce1fa48c8d8b7b5c184aa6e8792
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2021-03-09 18:04:56 +0100

    feat: add simple movement for the Pawns

--- Echecs/Domaine/Pion.cs
+++ Echecs/Domaine/Pion.cs
@@ -11,11 +11,63 @@ namespace Echecs.Domaine
     {
         public Pion(Joueur joueur) : base(joueur, TypePiece.Pion) { }
 
+        private bool premierDeplacement = true;
+
         public override bool Deplacer(Case destination)
         {
-            //destination.Link(this);
+            bool deplacementPossible = false;
 
-            return true;
+            // Blancs
+            if(this.info.couleur == CouleurCamp.Blanche)
+            {
+                Console.WriteLine("PremierDeplacementPossible(destination) = " + PremierDeplacementPossible(destination));
+                if(PremierDeplacementPossible(destination) || destination.col - this.position.col == -1 && destination.row - this.position.row == 0)
+                {
+                    deplacementPossible = true;
+                }
+            }
+
+            //Noirs
+            else
+            {
+                if(PremierDeplacementPossible(destination) || destination.col - this.position.col == 1 && destination.row - this.position.row == 0)
+                {
+                    deplacementPossible = true;
+                }
+            }
+
+            if (deplacementPossible && premierDeplacement)
+                premierDeplacement = false;
+
+            return deplacementPossible;
+        }
+
+        private bool PremierDeplacementPossible(Case destination)
+        {
+            bool deplacementPossible = false;
+
+            if(this.info.couleur == CouleurCamp.Blanche)
+            {
+                if(premierDeplacement 
+                    && destination.col - this.position.col == -2 
+                    && joueur.partie.echiquier.cases[this.position.row, this.position.col - 1].piece == null
+                    && joueur.partie.echiquier.cases[this.position.row, this.position.col - 2].piece == null)
+                {
+                    deplacementPossible = true;
+                }
+            }
+            else
+            {
+                if (premierDeplacement
+                    && destination.col - this.position.col == 2
+                    && joueur.partie.echiquier.cases[this.position.row, this.position.col + 1].piece == null
+                    && joueur.partie.echiquier.cases[this.position.row, this.position.col + 2].piece == null)
+                {
+                    deplacementPossible = true;
+                }
+            }
+
+            return deplacementPossible;
         }
     }
 }
