0e74366a4282cf85f97a3efa7941bbdee0ccf8cf
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2021-03-11 18:53:57 +0100

    feat: add castling movement to King

--- Echecs/Domaine/Roi.cs
+++ Echecs/Domaine/Roi.cs
@@ -22,14 +22,39 @@ namespace Echecs.Domaine
             int diffCol = destination.col - this.position.col;
             int diffRow = destination.row - this.position.row;
 
-            int rowToCheck = this.position.row;
-            int colToCheck = this.position.col;
-            int i = 0;
 
             if(Math.Abs(diffCol) <= 1 && Math.Abs(diffRow) <= 1)
             {
                 deplacementPossible = true;
             }
+            else if(this.premierDeplacement && diffCol == 0 && Math.Abs(diffRow) == 2)
+            {
+                Piece tour;
+                int rowActuel = this.position.row;
+                int colActuel = this.position.col;
+
+                if(diffRow == 2)
+                {
+                    tour = joueur.partie.echiquier.cases[7, colActuel].piece;
+
+                    deplacementPossible = tour != null && tour.GetType() == typeof(Tour) && tour.premierDeplacement
+                        && joueur.partie.echiquier.cases[rowActuel + 1, colActuel].piece == null
+                        && joueur.partie.echiquier.cases[rowActuel + 2, colActuel].piece == null;
+                }
+                else if (diffRow == -2)
+                {
+                    tour = joueur.partie.echiquier.cases[0, colActuel].piece;
+
+                    deplacementPossible = tour != null && tour.GetType() == typeof(Tour) && tour.premierDeplacement
+                        && joueur.partie.echiquier.cases[rowActuel - 1, colActuel].piece == null
+                        && joueur.partie.echiquier.cases[rowActuel - 2, colActuel].piece == null
+                        && joueur.partie.echiquier.cases[rowActuel - 3 , colActuel].piece == null;
+                }
+            }
+
+            if (this.premierDeplacement && deplacementPossible)
+                this.premierDeplacement = false;
+            
 
             return deplacementPossible;
         }
